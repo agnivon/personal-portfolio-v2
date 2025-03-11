9d487987176c206c7782b318361dd09d0dca0483
agnivon <agnivon@gmail.com>
2025-03-11 11:02:37 +0530

    feat: added analytics

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,9 +1,11 @@
 import { PropsWithChildren } from "react";
 import { ThemeProvider } from "./theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function AppProvider({ children }: PropsWithChildren) {
   return (
     <>
+      <Analytics />
       <ThemeProvider>{children}</ThemeProvider>
     </>
   );
