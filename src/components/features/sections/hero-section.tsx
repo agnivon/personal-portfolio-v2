24f6b88935f677a31398a243b46c7451dfe9dd2e
agnivon <agnivon@gmail.com>
2025-05-03 16:14:45 +0530

    refactor: removed vortex in hero

--- src/components/features/sections/hero-section.tsx
+++ src/components/features/sections/hero-section.tsx
@@ -59,7 +59,7 @@ export const HeroSpotlight = ({
   profile: PROFILES_QUERYResult[number];
 }) => {
   return (
-    <section className="h-screen w-full rounded-md flex flex-col items-center justify-center antialiased relative overflow-hidden snap-always snap-center bg-gradient-to-b from-black from-90% to-transparent">
+    <section className="h-screen w-full rounded-md flex flex-col items-center justify-center antialiased relative overflow-hidden snap-always snap-center">
       <Spotlight />
       <div className="w-full absolute inset-0 h-screen">
         {/* <SparklesCore
@@ -72,41 +72,33 @@ export const HeroSpotlight = ({
           particleColor="#FFFFFF"
         /> */}
       </div>
-      <Vortex
-        backgroundColor="transparent"
-        rangeY={800}
-        particleCount={500}
-        baseHue={120}
-        className="w-full h-full flex flex-col items-center justify-center"
-      >
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-          <p className="mb-4 font-semibold tracking-widest text-neutral-300 text-center mx-auto uppercase">
-            Agnivo Neogi
-          </p>
-          <GradientHeading>{profile.headline}</GradientHeading>
-          <p className="mt-4 font-normal md:text-xl text-neutral-300/90 max-w-lg xl:max-w-xl text-center mx-auto">
-            {profile.shortBio}
-          </p>
-        </div>
-        <div className="flex flex-col sm:flex-row items-center gap-x-4 mt-6">
-          {getSocialLinks(profile.socialLinks || {}).map(([key, value], id) => (
-            <a
-              key={key}
-              href={value}
-              target="_blank"
-              rel="noreferer noopener"
-              className="flex items-center gap-x-3 mb-5 hover:text-purple-400 duration-300"
-            >
-              <BorderMagicButton>
-                <div className="flex gap-1 items-center">
-                  <SocialMediaIcon value={key} />
-                  {key[0].toUpperCase() + key.toLowerCase().slice(1)}
-                </div>
-              </BorderMagicButton>
-            </a>
-          ))}
-        </div>
-      </Vortex>
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <p className="mb-4 font-semibold tracking-widest text-neutral-300 text-center mx-auto uppercase">
+          Agnivo Neogi
+        </p>
+        <GradientHeading>{profile.headline}</GradientHeading>
+        <p className="mt-4 font-normal md:text-xl text-neutral-300/90 max-w-lg xl:max-w-xl text-center mx-auto">
+          {profile.shortBio}
+        </p>
+      </div>
+      <div className="flex flex-col sm:flex-row items-center gap-x-4 mt-6">
+        {getSocialLinks(profile.socialLinks || {}).map(([key, value], id) => (
+          <a
+            key={key}
+            href={value}
+            target="_blank"
+            rel="noreferer noopener"
+            className="flex items-center gap-x-3 mb-5 hover:text-purple-400 duration-300"
+          >
+            <BorderMagicButton>
+              <div className="flex gap-1 items-center">
+                <SocialMediaIcon value={key} />
+                {key[0].toUpperCase() + key.toLowerCase().slice(1)}
+              </div>
+            </BorderMagicButton>
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
